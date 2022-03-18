9a2f7254382dbf9381380bbf8aeff692887cf1f7
Jashmin Patel <jasmin.3998@gmail.com>
2022-03-18 14:30:16 -0700

    Initial Commit

---
+++ stockTradingSystem/src/main/java/com/stockTrading/stockTradingSystem/StockTradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.stockTrading.stockTradingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTradingSystemApplication.class, args);
+	}
+
+}
---
+++ stockTradingSystem/src/test/java/com/stockTrading/stockTradingSystem/StockTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockTrading.stockTradingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
